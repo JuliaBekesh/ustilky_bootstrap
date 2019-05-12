5cb85eb39e756d75300d8d9da357cd5b6516c516
Julia Bekesh <julia.bekesh@gmail.com>
2019-05-12 17:47:12 +0000

    Description of the work done

---
+++ js/index.js
@@ -0,0 +1,13 @@
+document.querySelector('.login-form input[type=submit]').addEventListener('click',
+  submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+  fetch('form.php', {
+      method: 'POST',
+      body: new FormData(document.querySelector('.login-form'))
+    })
+    .then(response => response.text())
+    .then(html => document.querySelector('.server-response')
+      .innerHTML = html);
+}
