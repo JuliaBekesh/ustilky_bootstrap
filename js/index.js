b3c7c70350a432e716835f3329861992e85dfb32
Julia Bekesh <julia.bekesh@gmail.com>
2019-05-13 21:00:37 +0000

    Description of the work done

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
 document.querySelector('.login-form input[type=submit]').addEventListener('click',
-  submitForm);
+    submitForm);
 
 function submitForm(e) {
-  e.preventDefault();
-  fetch('form.php', {
-      method: 'POST',
-      body: new FormData(document.querySelector('.login-form'))
-    })
-    .then(response => response.text())
-    .then(html => document.querySelector('.server-response')
-      .innerHTML = html);
+    e.preventDefault();
+    fetch('form.php', {
+            method: 'POST',
+            body: new FormData(document.querySelector('.login-form'))
+        })
+        .then(response => response.text())
+        .then(html => document.querySelector('.server-response')
+            .innerHTML = html);
 }
+
+const clockContainer = document.querySelector('.clock');
+setInterval(() => clockContainer.innerText = (new Date()).toLocaleTimeString(), 1000);
+
